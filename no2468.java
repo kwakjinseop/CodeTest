6c7f7395990d64c877414c6edf4674366556dede
kwakjinseop <xmcdk@naver.com>
2021-01-20 22:08:06 +0900

    no1389

---
+++ no2468.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+import jdk.internal.org.jline.utils.InputStreamReader;
+
+import java.io.*;
+
+public class no2468{
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String x = br.readLine();
+        
+}
