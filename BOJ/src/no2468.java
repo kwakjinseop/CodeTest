d875aae649676f87dd8ab0aad63af5862bad7fc0
kwakjinseop <xmcdk@naver.com>
2021-01-26 11:13:08 +0900

    no2468

--- BOJ/src/no2468.java
+++ BOJ/src/no2468.java
@@ -16,26 +16,22 @@ public class no2468 {
 	static int board[][] = new int[N][N];
 	static int A1[] = new int[] { -1, 0, 1, 0 };
 	static int B1[] = new int[] { 0, 1, 0, -1 };
-	static Queue <Batch> que = new LinkedList<Batch>();
+	static Queue<Batch> que = new LinkedList<Batch>();
 
 	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String x = br.readLine();
-		N = Integer.parseInt(x);
+		Scanner input = new Scanner(System.in);
+		int N = input.nextInt();
 		board = new int[N][N];
 		for (int i = 0; i < N; i++) {
-			String xx = br.readLine();
 			for (int j = 0; j < N; j++) {
-				StringTokenizer st = new StringTokenizer(xx, " ");
-				board[i][j] = Integer.parseInt(st.nextToken());
+				board[i][j] = input.nextInt();
 
 			}
 		}
-		
-	
-		int result=0;
+
+		int result = 0;
 		for (int i = 0; i <= 100; i++) {
-			int cnt=0;
+			int cnt = 0;
 			boolean visited[][] = new boolean[N][N];
 			for (int j = 0; j < N; j++) {
 				for (int k = 0; k < N; k++) {
@@ -47,8 +43,8 @@ public class no2468 {
 			for (int j = 0; j < N; j++) {
 				for (int k = 0; k < N; k++) {
 					if (!visited[j][k]) {
+						que.offer(new Batch(j, k));
 						visited[j][k] = true;
-						que.add(new Batch(j, k));
 						while (!que.isEmpty()) {
 							Batch now = que.peek();
 							que.poll();
@@ -57,19 +53,18 @@ public class no2468 {
 								int ny = now.y + B1[u];
 								if (nx >= 0 && nx < N && ny >= 0 && ny < N && !visited[nx][ny]) {
 									visited[nx][ny] = true;
-									que.add(new Batch(nx, ny));
+									que.offer(new Batch(nx, ny));
 								}
 							}
 						}
 						cnt++;
 					}
-					
 				}
 			}
-			
-			result=Math.max(result, cnt);
+
+			result = Math.max(cnt ,result);
 		}
-		
+
 		System.out.println(result);
 
 	}
